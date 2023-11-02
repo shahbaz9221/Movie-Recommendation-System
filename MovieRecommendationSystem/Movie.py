566850acbb34a62306ba40d06dedb0b3255923fe
shahbazraza <shahbaz.raza@ibisdev.tech>
2023-11-02 19:56:18 +0500

    added documentation

--- MovieRecommendationSystem/Movie.py
+++ MovieRecommendationSystem/Movie.py
@@ -1,53 +1,85 @@
 import numpy as np
 import pandas as pd
 import time
+import logging
 
-movies_data=pd.read_csv('Movie_Dataset/movies.dat',sep='::',names=['MovieId','Title','Genere'])
-users_data=pd.read_csv('Movie_Dataset/users.dat',sep='::',header=None)
-ratings_data=pd.read_csv('Movie_Dataset/ratings.dat',sep='::',names=['UserId','MovieId','Rating','timestamp'])
+# Configure the logging module
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
 
+def load_movie_data():
+    """
+    Load movie data from the CSV file.
+    Returns:
+        movies_data (DataFrame): Movie data with columns: MovieId, Title, Genre.
+    """
+    movies_data = pd.read_csv('Movie_Dataset/movies.dat', sep='::', names=['MovieId', 'Title', 'Genre'])
+    return movies_data
 
-movies_data.column=['MovieId','Title','Genere']
-movies=pd.merge(movies_data,ratings_data,on='MovieId')
-n_users = movies.UserId.unique().shape[0]
-n_items = movies.Title.unique().shape[0]
-ratings = np.zeros((n_users, n_items))
-movie_user = movies.pivot_table(index='UserId', columns='Title', values='Rating')
-#print(moviemat.head())
+def load_user_data():
+    """
+    Load user data from the CSV file.
+    Returns:
+        users_data (DataFrame): User data with default columns.
+    """
+    users_data = pd.read_csv('Movie_Dataset/users.dat', sep='::', header=None)
+    return users_data
 
+def load_ratings_data():
+    """
+    Load ratings data from the CSV file.
+    Returns:
+        ratings_data (DataFrame): Ratings data with columns: UserId, MovieId, Rating, Timestamp.
+    """
+    ratings_data = pd.read_csv('Movie_Dataset/ratings.dat', sep='::', names=['UserId', 'MovieId', 'Rating', 'Timestamp'])
+    return ratings_data
 
-print("\n\nThere are 2 Recommendation System")
-print("  1: Item Base Recommendation System")
-print("  2: User Base Recommendation System\n")
-print("Press 1 for Item Base Recommendation ")
-print("Press 2 for User Base Recommendation ")
-x = int(input())
+def main():
+    movies_data = load_movie_data()
+    users_data = load_user_data()
+    ratings_data = load_ratings_data()
 
+    # Corrected column name assignment
+    movies_data.columns = ['MovieId', 'Title', 'Genre']
 
-while x!=0:
-    if x==1:
-        
-        selected_movie = movie_user['$1,000,000 Duck (1971)']
-        movies_corelation = movie_user.corrwith(selected_movie)
-        corr_DataFram = pd.DataFrame(movies_corelation, columns=['Correlation']) 
-        print("\nFollowing movies are liked by most of the users who liked this movie")
-        print("These movies are Recommended For You\n")
-        print(corr_DataFram.sort_values('Correlation', ascending=False).head())
-        
-    else:
-        
-        movie_user_Transpose=movie_user.T
-        selected_user=movie_user_Transpose[8]
-        users_corelation = movie_user_Transpose.corrwith(selected_user)
-        corr_User = pd.DataFrame(users_corelation, columns=['Correlation'])
-        print("\nFollowing Users are similar to selected User")
-        print(corr_User.sort_values('Correlation', ascending=False).head())
-        
-        
-    print("\n\nPress 1 for Item Base Recommendation ")
-    print("Press 2 for User Base Recommendation ")
-    print("Press 0 To Exit")
-    x = int(input())
+    # Merge movie and ratings data
+    movies = pd.merge(movies_data, ratings_data, on='MovieId')
 
-print("Good Luck")
-time.sleep(2)
+    n_users = movies['UserId'].nunique()
+    n_items = movies['Title'].nunique()
+
+    # Create a ratings matrix
+    ratings = np.zeros((n_users, n_items))
+
+    # Create a movie-user pivot table
+    movie_user = movies.pivot_table(index='UserId', columns='Title', values='Rating')
+
+    logging.info("There are 2 Recommendation Systems:")
+    logging.info("  1: Item-Based Recommendation System")
+    logging.info("  2: User-Based Recommendation System")
+
+    while True:
+        user_choice = int(input("Select an option (1 for Item-Based, 2 for User-Based, 0 to exit): ")
+        if user_choice == 0:
+            break
+        elif user_choice == 1:
+            selected_movie = movie_user['$1,000,000 Duck (1971)']
+            movies_correlation = movie_user.corrwith(selected_movie)
+            correlation_df = pd.DataFrame(movies_correlation, columns=['Correlation'])
+            logging.info("Movies liked by users who liked the selected movie:")
+            logging.info("Recommended movies for you:")
+            logging.info(correlation_df.sort_values('Correlation', ascending=False).head())
+        elif user_choice == 2:
+            movie_user_transpose = movie_user.T
+            selected_user = movie_user_transpose[8]
+            users_correlation = movie_user_transpose.corrwith(selected_user)
+            correlation_user = pd.DataFrame(users_correlation, columns=['Correlation'])
+            logging.info("Users similar to the selected user:")
+            logging.info(correlation_user.sort_values('Correlation', ascending=False).head())
+        else:
+            logging.error("Invalid option. Please select 1, 2, or 0 to exit.")
+
+    logging.info("Good Luck")
+    time.sleep(2)
+
+if __name__ == '__main__':
+    main()
